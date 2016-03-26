4fa6d7e5da1920a5c32c264fa611a2a3423e2fb7
Sebastian S <basti@localhost.localdomain>
2016-03-26 11:24:41 +0100

    Linux

--- Software/Android/NativeMemory/app/src/main/obj/local/armeabi/objs/Anmemdlopen/anmemdlopen.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/Anmemdlopen/anmemdlopen.o: \
- jni/Anmemdlopen/anmemdlopen.cpp jni/Anmemdlopen/anmemdlopen.h
-
-jni/Anmemdlopen/anmemdlopen.h:
--- Software/Android/NativeMemory/app/src/main/obj/local/mips64/objs/Anmemdlopen/anmemdlopen.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/mips64/objs/Anmemdlopen/anmemdlopen.o: \
- jni/Anmemdlopen/anmemdlopen.cpp jni/Anmemdlopen/anmemdlopen.h
-
-jni/Anmemdlopen/anmemdlopen.h:
