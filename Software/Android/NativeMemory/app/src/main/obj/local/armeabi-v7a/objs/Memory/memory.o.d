c2554d1e192ee58f57818d068546c686ac0dc180
Sebastian Schleemilch <sebastian.schleemilch@tum.de>
2016-04-07 14:27:56 +0200

    07_04

---
+++ Software/Android/NativeMemory/app/src/main/obj/local/armeabi-v7a/objs/Memory/memory.o.d
@@ -0,0 +1,9 @@
+obj/local/armeabi-v7a/objs/Memory/memory.o: jni/Memory/memory.cpp \
+ jni/Memory/schleemilch_ma_nativememory_MyNDK.h jni/Memory/openssl/aes.h \
+ jni/Memory/openssl/opensslconf.h
+
+jni/Memory/schleemilch_ma_nativememory_MyNDK.h:
+
+jni/Memory/openssl/aes.h:
+
+jni/Memory/openssl/opensslconf.h:
